740e58b2f855b074362a2db0560d2f0a0f40c92e
Francis Ajay D S <francisajay.ds@laderatechnology.com>
2024-07-24 22:46:11 +0530

    code

--- utclCustomer/srv/src/main/java/com/utcl/customer/handler/getCustomer.java
+++ utclCustomer/srv/src/main/java/com/utcl/customer/handler/getCustomer.java
@@ -1,14 +1,11 @@
 package com.utcl.customer.handler;
 
-import java.util.List;
-
-import javax.naming.spi.DirStateFactory.Result;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.sap.cds.Result;
 import com.sap.cds.ql.Select;
 import com.sap.cds.ql.cqn.CqnSelect;
 import com.sap.cds.services.handler.EventHandler;
@@ -16,85 +13,50 @@ import com.sap.cds.services.handler.annotations.On;
 import com.sap.cds.services.handler.annotations.ServiceName;
 import com.sap.cds.services.persistence.PersistenceService;
 
-import cds.gen.utclcustomer.Dealer;
 import cds.gen.utclcustomer.Dealer_;
+import cds.gen.utclcustomer.Dealer;
 import cds.gen.utclcustomer.GetdealerandretalerContext;
-import cds.gen.utclcustomer.Retailer_;
 import cds.gen.utclcustomer.UTCLCustomer_;
 
+import java.util.List;
 
 @Component
 @ServiceName(UTCLCustomer_.CDS_NAME)
-public class getCustomer implements EventHandler{
+public class getCustomer implements EventHandler {
 
     @Autowired
     private PersistenceService persistenceService;
 
-    public Logger log = LoggerFactory.getLogger(getCustomer.class);
+    private static final Logger log = LoggerFactory.getLogger(getCustomer.class);
 
     @On(event = GetdealerandretalerContext.CDS_NAME)
-    public void getDetails(GetdealerandretalerContext context){
+    public void getDetails(GetdealerandretalerContext context) {
 
         String id = context.getId();
 
         System.out.println(id);
 
-        log.info("Cusotmer id : " + id);
-
-        CqnSelect selectDealer = Select.from(Dealer_.class).where(o -> o.dealerID().eq(id));
+        log.info("Customer id to process : " + id);
 
         CqnSelect demoSelect = Select.from(Dealer_.class)
-                .columns(a -> a.id(), a -> a.firstName(),
-                        a -> a.lastName(),
-                        a -> a.email(),
-                        a -> a.mobileNumber(),
-                        a -> a.dealerID(),
-                        a -> a.toAddress().expand(d -> d.addressLine1(),
-                                d -> d.addressLine2(),
-                                d -> d.addressLine3(),
-                                d -> d.addressLine4(),
-                                d -> d.city(),
-                                d -> d.country(),
-                                d -> d.mobileNumber(),
-                                d -> d.pincode(),
-                                d -> d.state()),
-                        a -> a.toRetailer().expand(b -> b.retailerId(),
-                                b -> b.email(),
-                                b -> b.firstName(),
-                                b -> b.mobileNumber(),
-                                b -> b.id(),
-                                b -> b.lastName(),
-                                b -> b.toDealer_id(),
-                                b -> b.toAddress().expand(c -> c.addressLine1(),
-                                        c -> c.addressLine2(),
-                                        c -> c.addressLine3(),
-                                        c -> c.addressLine4(),
-                                        c -> c.city(),
-                                        c -> c.country(),
-                                        c -> c.mobileNumber(),
-                                        c -> c.pincode(),
-                                        c -> c.state())))
+                .columns(a -> a.id(), a -> a.firstName(), a -> a.lastName(), a -> a.email(), a -> a.mobileNumber(), a -> a.dealerID(),
+                        a -> a.toAddress().expand(d -> d.addressLine1(), d -> d.addressLine2(), d -> d.addressLine3(), d -> d.addressLine4(),
+                                d -> d.city(), d -> d.country(), d -> d.mobileNumber(), d -> d.pincode(), d -> d.state()),
+                        a -> a.toRetailer().expand(b -> b.retailerId(), b -> b.email(), b -> b.firstName(), b -> b.mobileNumber(), b -> b.id(),
+                                b -> b.lastName(), b -> b.toDealer_id(),
+                                b -> b.toAddress().expand(c -> c.addressLine1(), c -> c.addressLine2(), c -> c.addressLine3(),
+                                        c -> c.addressLine4(), c -> c.city(), c -> c.country(),
+                                        c -> c.mobileNumber(), c -> c.pincode(), c -> c.state())))
                 .where(o -> o.id().eq(id));
 
-        CqnSelect selectRetailer = Select.from(Retailer_.class).where(o -> o.toDealer_id().eq(id));
+        log.info("The Query Executed : " + demoSelect);
 
-        // CqnSelect joindandr = select.asJoin().left();
+        Result result = persistenceService.run(demoSelect);
 
-        System.out.println("The Query Executed : " + demoSelect);
+        log.info("Query Result : " + result);
 
-        System.out.println("The Query for Dealer : " + selectDealer);
+        List<Dealer> dealers = result.listOf(Dealer.class);
 
-        System.out.println("The Query for Retailer :  " + selectRetailer);
-
-        // Result result = persistenceService.run(demoSelect);
-
-        // System.out.println("The resulted query : " + result);
-
-        // List<Dealer> dealers = result.(Dealer.class);
-
-        // context.setResult(dealers);
-
-        
+        context.setResult(dealers);
     }
-    
 }
